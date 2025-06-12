8b04dd9a62baa31662e5bb988f11310893e9cb7f
Dino-code94 <dinoklaric29@gmail.com>
2025-06-12 06:02:13 -0700

    Backend: polished admin_views.py for delivery (PEP8, blank lines, comments)

--- core/admin_views.py
+++ core/admin_views.py
@@ -3,12 +3,18 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
 
+
+# API view to list all users (admin only)
 @api_view(['GET'])
 @permission_classes([IsAdminUser])
 def list_users(request):
-    users = list(User.objects.values('id', 'username', 'email', 'is_staff', 'is_superuser'))
+    users = list(User.objects.values(
+        'id', 'username', 'email', 'is_staff', 'is_superuser'
+    ))
     return Response(users)
 
+
+# API view to delete a user by ID (admin only)
 @api_view(['DELETE'])
 @permission_classes([IsAdminUser])
 def delete_user(request, user_id):
